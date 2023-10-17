d5f5a63a30d5e20586be33bd81ed6c2cc747e838
Jason Barry <jb@netlify.com>
2023-10-17 09:24:42 -0700

    Fix typescript errors

--- src/context/DataProvider.tsx
+++ src/context/DataProvider.tsx
@@ -1,13 +1,31 @@
 // import { useState } from 'react';
 import StoreContext from '~/context/store';
-// import type { Book, Swag } from '~/types/interfaces';
+import type { Book, Swag } from '~/types/interfaces';
 
 interface Props {
   children: React.ReactNode;
 }
 
 function StoreProvider({ children }: Props) {
-  return <StoreContext.Provider value={{}}>{children}</StoreContext.Provider>;
+  const books = [] as Book[];
+  const swag = [] as Swag[];
+
+  const fetchBooks = async () => {};
+
+  const fetchSwag = async () => {};
+
+  return (
+    <StoreContext.Provider
+      value={{
+        books,
+        swag,
+        fetchBooks,
+        fetchSwag,
+      }}
+    >
+      {children}
+    </StoreContext.Provider>
+  );
 }
 
 export default StoreProvider;
--- src/context/store.ts
+++ src/context/store.ts
@@ -1,7 +1,19 @@
 import { createContext } from 'react';
 
-// import type { Book, Swag } from '~/types/interfaces';
+import type { Book, Swag } from '~/types/interfaces';
 
-const StoreContext = createContext({});
+type Store = {
+  books: Book[];
+  swag: Swag[];
+  fetchBooks: (slug?: string) => void;
+  fetchSwag: () => void;
+};
+
+const StoreContext = createContext<Store>({
+  books: [],
+  swag: [],
+  fetchBooks: () => {},
+  fetchSwag: () => {},
+});
 
 export default StoreContext;
